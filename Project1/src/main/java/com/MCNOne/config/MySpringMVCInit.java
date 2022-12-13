f8a9e6a3c93cc06c9697660ff3b8eea1f02f1e1e
Havrylets Volodymyr <yadolovec@gmail.com>
2022-12-13 20:05:19 +0200

    MVC 2.3 no xml only java

---
+++ Project1/src/main/java/com/MCNOne/config/MySpringMVCInit.java
@@ -0,0 +1,20 @@
+package com.MCNOne.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
