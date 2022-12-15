3354662c1be583cb425e887bd8409c79fa0381b7
Havrylets Volodymyr <yadolovec@gmail.com>
2022-12-15 15:44:14 +0200

    MVC 2.6 @RequestParam and some Http with parametrs &? and so on

--- Project1/src/main/java/com/MCNOne/controllers/FirstController.java
+++ Project1/src/main/java/com/MCNOne/controllers/FirstController.java
@@ -2,17 +2,26 @@ package com.MCNOne.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surename = request.getParameter("surename");
+        System.out.println("Hello you, "+name+" "+surename+"!?");
         return "first/hello";
     }
 
     @GetMapping ("/goodbye")
-    public String goodBye(){
+    public String goodBye(@RequestParam(value="name", required = false) String name,
+                          @RequestParam(value="surename", required = false) String surename){
+        System.out.println("Bye you, "+name+" "+surename+"!?");
         return "first/goodBye";
     }
 }
