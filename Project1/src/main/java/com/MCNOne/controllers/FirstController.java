315f19022b8c5fc19dbbbf12bfd7f69f0071847a
Havrylets Volodymyr <yadolovec@gmail.com>
2022-12-17 10:48:40 +0200

    2.7 MVC Model, th:text, transfer data from controller to view by Model, made calculator

--- Project1/src/main/java/com/MCNOne/controllers/FirstController.java
+++ Project1/src/main/java/com/MCNOne/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.MCNOne.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.support.HttpRequestHandlerServlet;
@@ -20,8 +21,35 @@ public class FirstController {
 
     @GetMapping ("/goodbye")
     public String goodBye(@RequestParam(value="name", required = false) String name,
-                          @RequestParam(value="surename", required = false) String surename){
-        System.out.println("Bye you, "+name+" "+surename+"!?");
+                          @RequestParam(value="surename", required = false) String surename,
+                          Model model){
+        model.addAttribute("message","Bye you, "+name+" "+surename+"!?");
         return "first/goodBye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(HttpServletRequest request, Model model){
+        float a = Float.parseFloat(request.getParameter("a"));
+        float b = Float.parseFloat(request.getParameter("b"));
+        String action = request.getParameter("action");
+        String answer = doCalc(a,b,action);
+        model.addAttribute("answer", answer);
+
+        return "first/calculator";
+    }
+
+    public String doCalc(float a, float b, String action){
+        if (action.equals("divide")){
+            return a/b+"";
+        } else
+        if (action.equals("plus")){
+            return a+b+"";
+        } else
+        if (action.equals("minus")){
+            return a-b+"";
+        } else
+        if (action.equals("multiple")){
+            return a*b+"";
+        } else return "error, no such action";
     }
 }
