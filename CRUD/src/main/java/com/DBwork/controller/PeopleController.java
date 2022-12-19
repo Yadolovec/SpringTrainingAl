ff312de28d7863cfc8f8ffcdc83012c8ca5fa2e2
Havrylets Volodymyr <yadolovec@gmail.com>
2022-12-19 12:38:46 +0200

    MVC 2.9 HTML forms @ModelAtribute create so on

--- CRUD/src/main/java/com/DBwork/controller/PeopleController.java
+++ CRUD/src/main/java/com/DBwork/controller/PeopleController.java
@@ -1,12 +1,12 @@
 package com.DBwork.controller;
 
 import com.DBwork.dao.PersonDAO;
+import com.DBwork.models.Person;
+//import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +31,18 @@ public class PeopleController {
         //return one person by id
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
